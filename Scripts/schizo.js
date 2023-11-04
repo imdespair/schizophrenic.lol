b62b2caa542ba3ac615b896d42d0e140ac8d02ae
imdespair <immahutaosimp@gmail.com>
2023-11-04 08:58:13 +0000

    updated

---
+++ Scripts/schizo.js
@@ -0,0 +1,16 @@
+var discordEmbed = {
+    "embeds": [
+        {
+            "type": "link",
+            "url": "https://schizophrenic.lol/pass",
+            "title": "The Schizo Pass",
+            "description": "Use to your hearts content.",
+            "thumbnail": {
+                "url": "https://i.redd.it/ixamo1zuokaa1.png",
+                "proxy_url": "https://images-ext-2.discordapp.net/external/_ydviP8J8GBoTM8jbtxiy1hAyb9LbFdyKQj2XnmoMT8/https/i.redd.it/ixamo1zuokaa1.png",
+                "width": 755,
+                "height": 358
+            }
+        }
+    ]
+};
