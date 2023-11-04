978afe0d819e4a7eff8ad8fc6f589f83ce2c6123
imdespair <immahutaosimp@gmail.com>
2023-11-04 09:07:23 +0000

    idkkkkkk

--- Scripts/schizo.js
+++ Scripts/schizo.js
@@ -8,8 +8,8 @@ var discordEmbed = {
             "thumbnail": {
                 "url": "https://i.redd.it/ixamo1zuokaa1.png",
                 "proxy_url": "https://images-ext-2.discordapp.net/external/_ydviP8J8GBoTM8jbtxiy1hAyb9LbFdyKQj2XnmoMT8/https/i.redd.it/ixamo1zuokaa1.png",
-                "width": 755,
-                "height": 358
+                "width": 1200,
+                "height": 600
             }
         }
     ]
